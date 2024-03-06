096e81a26a61a6891b85dcee86df5685447e6158
Hevardhan.S <112763006+hevardhan@users.noreply.github.com>
2024-03-06 18:50:22 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,10 @@
+package ass4;
+
+public class Main {
+
+	    public static void main(String[] args) {
+	        Deck deck = new Deck();
+	        deck.createDeck(); // Create a deck of cards
+	        deck.displayMenu(); // Display the menu for the user
+	    }
+	}
